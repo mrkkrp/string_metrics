8a648232f308c54045e5d2b09d6eabf528f00292
mrkkrp <markkarpov92@gmail.com>
2019-09-24 16:03:57 +0200

    Make the basic test work locally

---
+++ tests/test_string_metrics.ml
@@ -0,0 +1,10 @@
+let test_x () =
+  Alcotest.(check int) "x is 5" String_metrics.magic_x 4
+
+let () =
+  Alcotest.run "string_metrics"
+    [ ( "magic x",
+        [ Alcotest.test_case "magic x" `Quick test_x
+        ]
+      )
+    ]
